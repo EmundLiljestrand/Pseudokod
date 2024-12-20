1c4c0e7630a8dfa3450abc9f961cd379bbad2764
EmundLiljestrand <emund.l@hotmail.com>
2024-12-20 14:06:13 +0100

    G klar

---
+++ script.js
@@ -0,0 +1,71 @@
+// G-Version - Split the Nota
+// Skriv din pseudokod innanför nedanstående kommentarsblock
+/*
+
+START 
+
+*get user input bill amount*
+print: "Ange totalbeloppet för notan."
+input: amount
+*check input: amount*
+IF amount <= 0 THEN print "Totalbeloppet måste vara större än 0kr. Försök igen."
+END IF
+
+*get user split count*
+print: "Ange antal vänner (exempelvis 4)."
+input: friends_amount
+*check input: friends_amount*
+IF friends_amount <= 0 THEN print: "Antal vänner måste vara större än 1. Försök igen."
+END IF
+
+*get user tip amount*
+print: "Ange dricksen i decimalform (exempelvis 0.10 för 10%)."
+input: tip
+*check input: tip*
+IF tip <= 0 THEN print: "Dricksen måste vara större än 0. Försök igen."
+END IF
+
+*calculate final amount*
+tip = amount * tip
+final_amount = amount + tip
+
+*calculate amount per friend*
+amount_per_friend = final_amount / friends_amount
+
+*present result to user*
+print: "Den totala summan inklusive dricks är "final_amount"."
+print: "Varje vän ska betala "amount_per_friend"."
+
+END
+
+================ Flödesdiagram =====================
+Figjam: https://www.figma.com/board/BdcyjvFR0UOzm4adXjsJSZ/Untitled?node-id=0-1&t=mGqXrfhgJGck85uP-1
+
+
+
+*/
+
+// VG-Version - Lewis Carroll Word Puzzle
+// Skriv din pseudokod innanför nedanstående kommentarsblock
+// Startkoden är 100% frivillig och kan tas bort eller skrivas om
+/*
+
+play();
+
+function play()
+    SET variabel ordbok = [FOUR, FOUL, FOOL, FOOT, FORT, FORE, FIRE, FIVE]; // Innehåller ALLA ord i det engelska språket
+    SET variabel startOrd till "FOUR";
+    SET variabel slutordOrd till "FIVE";
+
+
+end function
+
+function isOneLetterApart(wordOne, wordTwo)
+    SET variabel diffCount till 0;
+
+    // Här behöver du skriva koden för din funktion
+
+    return diffCount === 1; // returnerar sant om endast en bokstav ändrats, annars falskt
+end function
+
+*/
